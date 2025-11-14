a9e2d9003e266b3a4f54fde477235a360a6cb447
Cris <crisce4@hotmail.com>
2025-11-14 18:19:35 -0500

    Redirect /admin no welcome

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistroParticipanteController;
-Route::get('/', function () {
-    return view('welcome');
-});
-// Route::redirect('/', '/admin');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/admin');
 Route::get('/qr/{slug}', [RegistroParticipanteController::class, 'create'])
     ->name('participante.form');
 
